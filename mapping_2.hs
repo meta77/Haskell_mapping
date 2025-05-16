37f6d5d90c1ba843751ff5fb8662df2967939a4b
Meta77 <horokusa@keio.jp>
2025-05-16 21:51:38 +0900

    拡張された構造付き写像の定義

--- mapping_2.hs
+++ mapping_2.hs
@@ -3,3 +3,10 @@ class Mapping f where
   type Domain f
   type Codomain f
   apply :: f -> Domain f -> Codomain f
+
+-- 拡張された構造付き写像
+data Mapping a b = Mapping {
+  apply     :: a -> b,
+  domainSet :: a -> Bool,    -- 属するかどうか（∞集合でもOK）
+  codomainSet :: b -> Bool   -- 値域として含まれるか
+}
