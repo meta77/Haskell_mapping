c0e20db2f3262c133c2129e5bfd2fc6e99c36f1f
Meta77 <horokusa@keio.jp>
2025-05-16 23:33:22 +0900

    コメント　関連型

--- mapping_2.hs
+++ mapping_2.hs
@@ -1,8 +1,33 @@
+{-# LANGUAGE TypeFamilies #-}
+
+
 -- Mapping を型クラスで抽象化
 class Mapping f where
   type Domain f
   type Codomain f
   apply :: f -> Domain f -> Codomain f
+
+{-
+型クラスの中での、typeの意味
+
+「関連型（associated type）」を定義するため。
+
+「型 f（例えば関数やデータ構造など）に、それぞれ異なる**定義域（Domain）と値域（Codomain）**の型がある」
+→ それを明示的に型として指定したい！
+
+型クラスの中で type Something f のように書くと、
+それは 「型引数 f に関連付けられた別の型」 を定義する、という意味になります。
+これは普通の型パラメータではなく、型ファミリー（type family） という仕組みで実現されます。
+
+より自然に「型に対してその性質・構造を与える」ため
+
+この機能を使うには GHC 拡張：
+{-# LANGUAGE TypeFamilies #-}
+をソースコードの先頭に書く必要があります。
+
+
+-}
+
 
 -- 拡張された構造付き写像
 data Mapping a b = Mapping {
